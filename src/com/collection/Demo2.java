d23531dd6745135e3cd9002285b2c6ac92a7f8f6
BhagyashriDabhade <122371956+BhagyashriDabhade@users.noreply.github.com>
2023-01-11 23:05:02 +0530

    Update Demo2.java
    changes done..

--- src/com/collection/Demo2.java
+++ src/com/collection/Demo2.java
@@ -6,9 +6,6 @@ public class Demo2 {
 
 	public static void main(String[] args) {
 	List<Integer> al= new ArrayList<Integer>();
-	al.add(10);
-	al.add(20);
-	al.add(30);
 	al.add(40);
 	al.add(50);
 		 System.out.println(al);
